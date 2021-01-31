894311ddb7f9cd2ef2a9bd06f35b6bdf07564ed8
mohammadkz777 <callpfarcry@gmail.com>
2021-01-31 14:10:32 -0800

    getsyscallcountertest updated

--- getsyscallcountertest.c
+++ getsyscallcountertest.c
@@ -5,6 +5,7 @@
 
 int main(){
 
+    printf(1, "before fork:\n\n");
     for(int i = 0; i < 5; i++)
         getpid();
     printf(1, "counts of system call getpid calls : %d\n", getsyscallcounter(11));
@@ -13,9 +14,42 @@ int main(){
         getparentid();
     printf(1, "counts of system call getparentid calls : %d\n", getsyscallcounter(22));
 
+    printf(1, "================================\n");
+    
+    int pid = fork();
+    if(pid == 0){
+        for(int i=0; i < 4; i++)
+            getpid();
+        printf(1, "child process:\n");    
+        printf(1, "counts of system call getpid calls : %d\n", getsyscallcounter(11));
+        printf(1, "counts of system call fork calls : %d\n", getsyscallcounter(1));
+    }
+
+    else if(pid > 0)
+    {
+        wait();
+        printf(1, "parent process:\n");
+        printf(1, "counts of system call fork calls : %d\n", getsyscallcounter(1));
+        printf(1, "counts of system call wait calls : %d\n", getsyscallcounter(3));
+    }
+    else
+    {
+        printf(1, "fork failed\n");
+        exit();
+    }
+    
+
     for(int i = 0 ; i < 10; i++)
         getpid();
-    printf(1, "counts of system call getpid calls : %d\n", getsyscallcounter(11));   
+        
+    if(pid == 0){
+        printf(1, "child process:\n");
+        printf(1, "counts of system call getpid calls : %d\n", getsyscallcounter(11));   
+    }
+    else{
+        printf(1, "parent process:\n");
+        printf(1, "counts of system call getpid calls : %d\n", getsyscallcounter(11));  
+    }
 
     return 0;
 }
