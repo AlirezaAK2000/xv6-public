89a70322b50365a308473cedefdbbd06b2c525ad
aliak <a.akhoundi79@yahoo.com>
2021-02-05 18:17:08 +0330

    round robin test refactored and quantum changed

--- roundRobinTest.c
+++ roundRobinTest.c
@@ -20,10 +20,29 @@ void print_averages(metrics *sum)
     printf(1, "\n\n\n");
     printf(1, "_____________________________________________\n");
     printf(1, "averages: \n");
-    printf(1, "turnaround time : %2.f", (float)sum->turn_around / PROCESS_NUM);
-    printf(1, "wait time : %2.f", (float)sum->wait / PROCESS_NUM);
-    printf(1, "cpu burst time : %2.f", (float)sum->cbt / PROCESS_NUM);
+    printf(1, "turnaround time : %d\n", sum->turn_around / PROCESS_NUM);
+    printf(1, "wait time : %d\n", sum->wait / PROCESS_NUM);
+    printf(1, "cpu burst time : %d\n", sum->cbt / PROCESS_NUM);
     printf(1, "_____________________________________________\n");
+}
+
+void wait_for_children(){
+    int pids[PROCESS_NUM];
+    metrics **mat;
+    metrics *sum = malloc(sizeof(metrics));
+    mat = malloc(sizeof(metrics *) * PROCESS_NUM);
+    for (int i = 0; i < PROCESS_NUM; i++)
+        mat[i] = malloc(sizeof(metrics));
+    for (int i = 0; i < PROCESS_NUM; i++)
+    {
+        pids[i] = waitandgetmetrics(mat[i]);
+        sum->cbt += mat[i]->cbt;
+        sum->turn_around += mat[i]->turn_around;
+        sum->wait += mat[i]->wait;
+    }
+    for (int i = 0; i < PROCESS_NUM; i++)
+        print_metrics(mat[i], pids[i]);
+    print_averages(sum);
 }
 
 int main()
@@ -39,22 +58,7 @@ int main()
 
     if (pid == getpid())
     {
-        int pids[PROCESS_NUM];
-        metrics **mat;
-        metrics *sum = malloc(sizeof(metrics));
-        mat = malloc(sizeof(metrics *) * PROCESS_NUM);
-        for (int i = 0; i < PROCESS_NUM; i++)
-            mat[i] = malloc(sizeof(metrics));
-        for (int i = 0; i < PROCESS_NUM; i++)
-        {
-            pids[i] = waitandgetmetrics(mat[i]);
-            sum->cbt += mat[i]->cbt;
-            sum->turn_around += mat[i]->turn_around;
-            sum->wait += mat[i]->wait;
-        }
-        for (int i = 0; i < PROCESS_NUM; i++)
-            print_metrics(mat[i], pids[i]);
-        print_averages(sum);
+        wait_for_children();
     }
     else
     {
